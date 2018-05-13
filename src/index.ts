f3c184ae4f3fb04b9ba303f89f88c19af5f0b035
Pranjal Satija <pranjalsatija@users.noreply.github.com>
2018-05-12 21:49:59 -0400

    Replace https with http in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
-import * as https from "https";
+import * as http from "http";
 import * as routes from "./routes";
 
 require("dotenv").load();
@@ -9,6 +9,6 @@ const app = express.default();
 app.use(bodyParser.json());
 app.use("/auth", routes.default);
 
-https.createServer({}, app).listen(process.env.PORT, () => {
+http.createServer(app).listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}.`);
 });
